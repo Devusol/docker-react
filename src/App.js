3a3bf0763f8320bddc89dcee5379474b8b1a3c66
Sean <sean@devusol.com>
2023-06-25 07:31:00 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Changes for Docker 3. Running with volumes on WSL Ubuntu.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Docker
         </a>
       </header>
     </div>
